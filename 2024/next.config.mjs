f28dab90d006844e7a3910c6a234f7a11c18d02c
YZRay1119 <yao19991119@mopcon.org>
2024-08-22 17:32:47 +0800

    test

--- 2024/next.config.mjs
+++ 2024/next.config.mjs
@@ -2,6 +2,7 @@
 const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig = {
+  output: "export",
   basePath: isProduction ? "/2024" : undefined,
   trailingSlash: true,
   images: {
