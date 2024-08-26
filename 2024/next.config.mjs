88524a44da6e5ef1ea9a6c553d8977fd30d4edc9
YZRay1119 <yao19991119@mopcon.org>
2024-08-26 22:15:59 +0800

    test

--- 2024/next.config.mjs
+++ 2024/next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === "production";
 
-export default (phase) => {
-  const nextConfig = {
-    basePath: isProduction ? "/2024" : undefined,
-    trailingSlash: true,
-    images: {
-      unoptimized: true,
-    },
-    output: phase === "phase-production-build" ? "export" : "standalone",
-  };
-  return nextConfig;
+const nextConfig = {
+  basePath: isProduction ? "/2024" : undefined,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
+return nextConfig;
+
+export default nextConfig;
