fcc2810dd3f809a1a63c80012a65914e040a1c9d
YZRay1119 <yao19991119@mopcon.org>
2024-09-26 20:46:27 +0800

    test

--- 2024/src/app/time-machine/_ui/TimeMachineBlock.jsx
+++ 2024/src/app/time-machine/_ui/TimeMachineBlock.jsx
@@ -1,27 +1,10 @@
 "use client";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Navigation } from "swiper/modules";
-// import "swiper/css";
+
 import { SectionBlock, SectionTitle } from "@/components/ui/SectionBlock";
 import { getImageSrc } from "@/components/util/getImageSrc";
 import Image from "next/image";
-import TimeMachineCard from "../_components/TimeMachineCard";
-// import { ArrowRight, ArrowLeft } from "./SwiperArrow";
-import timeMachineData from "@/components/data/timeMachine.json";
-import { useState, useRef } from "react";
-import { AnimatePresence } from "framer-motion";
 
 const TimeMachineBlock = () => {
-  const [timeContent, setTimeContent] = useState(timeMachineData[0]);
-  // const swiperRef = useRef(null);
-  // const swiperChangeHandler = (swiper) => {
-  //   setTimeContent(timeMachineData[swiper.realIndex]);
-  // };
-  // const handleSlideClick = (index) => {
-  //   if (swiperRef.current && swiperRef.current.swiper) {
-  //     swiperRef.current.swiper.slideToLoop(index);
-  //   }
-  // };
   return (
     <SectionBlock className="relative bg-[#F4F7FA]">
       <Image
@@ -37,63 +20,7 @@ const TimeMachineBlock = () => {
           歷年 MOPCON
         </SectionTitle>
         <div className="flex flex-col-reverse tablet:flex-col">
-          <AnimatePresence mode="wait">
-            <TimeMachineCard key={timeContent.year} data={timeContent} />
-          </AnimatePresence>
-          <div className="relative tablet:w-1/2 tablet:mx-auto p-2 mb-8 [mask-image:linear-gradient(90deg,_transparent_0%,_#fff_30%,_#fff_70%,_transparent_100%)] tablet:[mask-image:none]">
-            {/* <Swiper
-              ref={swiperRef}
-              slidesPerView={5}
-              centeredSlides={true}
-              speed={440}
-              loop={true}
-              navigation={{
-                nextEl: `.time-next`,
-                prevEl: `.time-prev`,
-              }}
-              onSlideChange={(swiper) => {
-                swiperChangeHandler(swiper);
-              }}
-              breakpointsBase="container"
-              spaceBetween={24}
-              modules={[Navigation]}
-              on={{}}
-            >
-              {timeMachineData.map((item, index) => (
-                <SwiperSlide
-                  className="!flex my-auto"
-                  key={index}
-                  onClick={() => handleSlideClick(index)}
-                >
-                  {({ isActive }) => (
-                    <span
-                      className={`duration-300 font-medium cursor-pointer mx-auto ${
-                        isActive
-                          ? "text-[#1C1F25] text-xl tablet:text-2xl"
-                          : "text-N800/80 text-sm tablet:text-lg"
-                      }`}
-                    >
-                      {item.year}
-                    </span>
-                  )}
-                </SwiperSlide>
-              ))}
-            </Swiper> */}
-            {/* <button
-              type="button"
-              className="swiper-button-prev time-prev absolute left-0 top-1/2 -translate-y-1/2 -translate-x-full p-4 hidden tablet:inline-block"
-              aria-label="Previous Slide"
-            >
-              <ArrowLeft />
-            </button>
-            <button
-              type="button"
-              className="swiper-button-next time-next absolute right-0 top-1/2 -translate-y-1/2 translate-x-full p-4 hidden tablet:inline-block"
-              aria-label="Next Slide"
-            >
-              <ArrowRight />
-            </button> */}
-          </div>
+          <div className="relative tablet:w-1/2 tablet:mx-auto p-2 mb-8 [mask-image:linear-gradient(90deg,_transparent_0%,_#fff_30%,_#fff_70%,_transparent_100%)] tablet:[mask-image:none]"></div>
         </div>
       </div>
     </SectionBlock>
