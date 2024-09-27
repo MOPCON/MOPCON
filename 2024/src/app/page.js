92ee51c6bff247ebca20622ccb4b34ba74db78f2
YZRay1119 <yao19991119@mopcon.org>
2024-09-27 19:40:16 +0800

    update

--- 2024/src/app/page.js
+++ 2024/src/app/page.js
@@ -3,6 +3,7 @@ import About from "./_root/_ui/About";
 import Venue from "./_root/_ui/Venue";
 import NewHero from "./_root/_ui/NewHero";
 import MajorReason from "./_root/_ui/MajorReason";
+// import SponsorSwiper from "./_root/_components/SponsorSwiper";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <About />
       <Venue />
       <MajorReason />
+      {/* <SponsorSwiper /> */}
     </Fragment>
   );
 }
