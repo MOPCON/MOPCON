807c0b1024349e976cba2c4ffbeef0060b222d23
YZRay1119 <yao19991119@mopcon.org>
2024-09-29 17:41:53 +0800

    update information

--- 2024/src/app/community/page.jsx
+++ 2024/src/app/community/page.jsx
@@ -5,7 +5,7 @@ import JoinCommunity from "./_ui/JoinCommunity";
 import BehavioralGuidelines from "./_ui/BehavioralGuidelines";
 
 export const metadata = {
-  title: "主辦社群",
+  title: "主辦單位",
   description:
     "MOPCON是一個專注於濁水溪以南地區的科技社群。自創立以來，我們致力於為南台灣提供一個討論技術與交流的平台。我們深知南部地區的人才和技術資源豐富，但往往缺乏能夠匯聚這些力量的場域。因此，我們的目標是提供一個專注於南部社群的舞台，讓每位社群夥伴都能夠在這裡一同交流、成長，創造共好生態系。",
 };
--- 2024/src/app/schedule/page.jsx
+++ 2024/src/app/schedule/page.jsx
@@ -8,7 +8,7 @@ import { Suspense } from "react";
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata = {
-  title: "主要議程",
+  title: "議程介紹",
   description:
     "MOPCON 2024，將帶你探索技術之樹的每一條分枝，從基礎知識到前沿趨勢，無論你是哪一個分枝的探險者，都能在這裡找到屬於自己的知識之路，親身感受技術的脈動、在未來之林中開創屬於自己的方向。",
 };
