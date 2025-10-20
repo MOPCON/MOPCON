54930fbb912851cf944bf0cb617675e0c22ebc19
Yorukot <hi@yorukot.me>
2025-10-20 22:18:59 +0800

    feat: update something to fix the error

--- 2025/astro.config.mjs
+++ 2025/astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; 
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  base: '/2025',
-  trailingSlash: 'always',
+  base: "/2025",
+  trailingSlash: "ignore",
 });
