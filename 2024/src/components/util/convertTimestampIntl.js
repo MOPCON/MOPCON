baf6db729f63e3168975bcb91eef86c9c3c9f9ba
Mikimoto <mikimoto.c@gmail.com>
2026-05-05 11:35:31 +0800

    feat(phase3): replace 2024/ and 2025/ source with build artifact

--- 2024/src/components/util/convertTimestampIntl.js
+++
@@ -1,8 +0,0 @@
-export function convertTimestampIntl(timestamp, option, locale = "zh-TW") {
-  return new Intl.DateTimeFormat(locale, {
-    ...option,
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  }).format(new Date(timestamp * 1000));
-}
